ab9dde9758f5f8efe692af7d35178d8bc6fcf975
marisasaunders <marisaksaunders@gmail.com>
2022-03-29 20:33:01 -0400

    POST / question deliverable

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -19,6 +19,13 @@ function QuestionForm(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
+    fetch("http://localhost:4000/questions", {
+      method: "POST",
+      headers: { 
+        'Content-Type': "application/json", 
+      },
+      body: JSON.stringify(formData)
+    })
     console.log(formData);
   }
 
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+    .then((r) => r.json())
+    .then((questions) => setQuestions(questions));
+  })
+
+  const questionItems = questions.map((q) => (
+    <QuestionItem
+      key={q.id}
+      question={q}
+    />
+  ));
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questionItems}</ul>
     </section>
   );
 }
